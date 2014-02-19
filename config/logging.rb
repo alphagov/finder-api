ab50ad6bcab7eaf9f3035c87fe43a223340d7e55
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-02-19 17:09:22 +0000

    Configure default rake task to run tests
    also change jenkins script to invoke default rake task

--- config/logging.rb
+++ config/logging.rb
@@ -1,5 +1,8 @@
+require 'fileutils'
+
 before {
   error_log_file = File.expand_path("../log/production.log", File.dirname(__FILE__))
+  FileUtils.mkdir_p(File.dirname(error_log_file))
   error_log = File.open(error_log_file, 'a+')
   error_log.sync = true
   env['rack.errors'] = error_log
