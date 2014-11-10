b543fcbbe48280b43f41159ea80030624253da50
Tom Russell <tom@cishub.co.uk>
2014-11-10 11:02:23 +0000

    Add copy for metadata choices to drug-device-alerts
    This adjusts the format of the metadata for the signup choices so each choice
    can contain more information

--- lib/email_list_permutation_generator.rb
+++ lib/email_list_permutation_generator.rb
@@ -6,13 +6,21 @@ class EmailListPermutationGenerator
   end
 
   def all_possible_tag_hashes
-    each_combination_of(choices_hash["choices"]).map do |combination|
-      {"format" => [format], choices_hash["key"] => combination}
+    each_combination_of(available_choices).map do |combination|
+      {"format" => [format], choice_key => combination}
     end
   end
 
 private
   attr_reader :format, :choices_hash
+
+  def available_choices
+    choices_hash["choices"].map { |entry| entry["key"] }
+  end
+
+  def choice_key
+    choices_hash["key"]
+  end
 
   # Creates a powerset for every possible combination of a set of options
   def each_combination_of(value)
